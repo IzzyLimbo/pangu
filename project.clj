523c18ae6d1109c7f9a1a619e7984b133f19df7f
Izzy Limbo <izzylimbo@gmail.com>
2012-04-23 16:35:24 +0800

    1. Create new branch for heroku publish. 2. Change :pretty-print to false in project.clj. 3. Comment the :cljsbuild option :output-dir in project.clj, this is only for debug.

--- project.clj
+++ project.clj
@@ -1,24 +1,24 @@
 (defproject pangu "0.0.1"
   :description "Birdthings Pangu Project."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [jayq "0.1.0-SNAPSHOT"]
-                 [crate "0.1.0-SNAPSHOT"]
-                 [fetch "0.1.0-SNAPSHOT"]
+                 [jayq "0.1.0-alpha3"]
+                 [crate "0.1.0-alpha3"]
+                 [fetch "0.1.0-alpha2"]
                  [noir "1.2.2"]]
-  :plugins [[lein-cljsbuild "0.1.7"]]
+  :plugins [[lein-cljsbuild "0.1.8"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds [
               {:source-path "src/pangu/cljs/main",
                 :compiler {
-               ; :output-dir "resources/public/cljs/main",  ;; for debug mode
+                 ; :output-dir "resources/public/cljs/main",  ;; for debug mode
                  :output-to "resources/public/js/main.js",
                  :optimizations :simple,
-                 :pretty-print true}}
+                 :pretty-print false}}
               {:source-path "src/pangu/cljs/welcome",
                 :compiler {
-               ; :output-dir "resources/public/cljs/welcome",  ;; for debug mode
+                 ; :output-dir "resources/public/cljs/welcome",  ;; for debug mode
                  :output-to "resources/public/js/welcome.js",
                  :optimizations :simple,
-                 :pretty-print true}}
+                 :pretty-print false}}
                  ]}
   :main pangu.server)
