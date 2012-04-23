d7f100643e304e5b6e4f53bf41e6a8850e83f6a1
Izzy Limbo <izzylimbo@gmail.com>
2012-04-23 16:57:28 +0800

    Update :dependencies and :plugins version in project.clj.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject pangu "0.0.1"
   :description "Birdthings Pangu Project."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [jayq "0.1.0-SNAPSHOT"]
-                 [crate "0.1.0-SNAPSHOT"]
-                 [fetch "0.1.0-SNAPSHOT"]
+                 [jayq "0.1.0-alpha3"]
+                 [crate "0.1.0-alpha3"]
+                 [fetch "0.1.0-alpha2"]
                  [noir "1.2.2"]]
-  :plugins [[lein-cljsbuild "0.1.7"]]
+  :plugins [[lein-cljsbuild "0.1.8"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds [
               {:source-path "src/pangu/cljs/main",
